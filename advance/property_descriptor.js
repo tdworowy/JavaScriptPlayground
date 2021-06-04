a1d3d26bc509294e0e6fa1e630a9c3d2bc531f3f
Tomasz <dworowytomasz@gmail.com>
2021-06-04 14:25:12 +0200

    update

---
+++ advance/property_descriptor.js
@@ -0,0 +1,13 @@
+let descriptor1 = Object.getOwnPropertyDescriptor({x:1},"x")
+console.log(descriptor1)
+
+let obj = {}
+Object.defineProperty(obj, "x", {
+    value: 2,
+    writable: true,
+    enumerable: false,
+    configurable: true
+})
+
+let descriptor2 = Object.getOwnPropertyDescriptor(obj,"x")
+console.log(descriptor2)
